1795f13d071d0e700c2be81ce5d78455362b478b
larissa <rocha.dbx@gmail.com>
2022-04-12 13:33:40 -0300

    agora os valores sao calculados todos uma só vez, no construtor

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -1,42 +1,58 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Optional;
 
 public class Operations {
     private final ArrayList<Integer> userInput;
     private double average;
     private int sizeOfEntry;
+    private int max;
+    private int min;
 
     public  Operations (ArrayList<Integer> entry) {
-        userInput = entry;
+        this.userInput = entry;
+
         setSizeOfEntry();
+        setMinimum();
+        setMaximum();
+        setAverage();
+
+        this.average = getAverage();
+        this.min = getMinimum();
+        this.max = getMaximum();
     }
 
-    public double getAverage(){
+    public void setAverage(){
+        double averg = 0;
         for (Integer integer : userInput) {
-            average += integer;
+            averg += integer;
         }
-        return (average/userInput.size());
+        this.average = (averg/this.userInput.size());
     }
 
-    public Integer getMaximum (){
+    public double getAverage(){ return this.average; }
+
+    public void setMaximum (){
         Optional<Integer> reduce = userInput.stream()
-                                            .reduce((n1, n2) -> n1 > n2 ? n1 : n2);
-        return reduce.isPresent() ? reduce.get(): null;
+                                            .reduce((accumulator, total) -> accumulator > total ? accumulator : total);
+        this.max = reduce.isPresent() ? reduce.get(): 0;
     }
 
-    public Integer getMinimum (){
+    public Integer getMaximum(){ return this.max; }
+
+    public void setMinimum (){
         Optional<Integer> reduce = userInput.stream()
-                                            .reduce((n1, n2) -> n1 < n2 ? n1 : n2);
-        return reduce.isPresent() ? reduce.get(): null;
+                                            .reduce((accumulator, total) -> accumulator < total ? accumulator : total);
+        this.min = reduce.isPresent() ? reduce.get(): 0;
     }
+
+    public Integer getMinimum(){ return this.min; }
 
     public void setSizeOfEntry() {
-        this.sizeOfEntry = userInput.size();
+        this.sizeOfEntry = this.userInput.size();
     }
 
     public int getSizeOfEntry() {
-        return sizeOfEntry;
+        return this.sizeOfEntry;
     }
 
 }
